4726679d12b0f69d668040ba29aedd4b6410a660
Henrique Nichii Lopes <henrique.nichii@gmail.com>
2025-10-27 20:01:49 -0300

    feat: Adiciona ficheiro de entrada wsgi para o Gunicorn

--- Integração/venv/Scripts/activate.bat
+++ Integração/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\henri\OneDrive\Área de Trabalho\ERP\Integração\venv
+set VIRTUAL_ENV=C:\Users\henri\OneDrive\Área de Trabalho\ERP2\Integração\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
